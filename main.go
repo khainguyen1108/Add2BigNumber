e683df258281ef11c1a89859348ea1ab7c41ee17
khai <khai11080707@gmail.com>
2026-03-11 21:18:01 +0700

    *feat: add logging

--- main.go
+++ main.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	add2num "ADD2NUM/add2num"
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
+
+	// tao file log
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println("[main.file.err]", err)
+		return
+	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Number1: ")
+	a, _ := reader.ReadString('\n')
+
+	fmt.Print("Number2: ")
+	b, _ := reader.ReadString('\n')
+
+	a = strings.TrimSpace(a)
+	b = strings.TrimSpace(b)
+
 	myBigNumber := add2num.MyBigNumber{}
-	fmt.Println(myBigNumber.Sum("456", "789"))
+	result := myBigNumber.Sum(a, b)
+
+	fmt.Println("Result:", result)
+
+	log.Println("Input 1:", a, "Input 2:", b, "Result:", result)
 }
